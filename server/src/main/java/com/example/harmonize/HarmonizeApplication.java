51b0ba1727d14a7f503223dbaeed2e1a56d3ef0b
sori9899 <hyun989969@gmail.com>
2023-04-12 16:53:18 +0900

    App Project Backend

---
+++ server/src/main/java/com/example/harmonize/HarmonizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.harmonize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonizeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/harmonize/HarmonizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harmonize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
